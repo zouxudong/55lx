bcd2eae2d0662b087c7d2d36f0fcad5a86695f4c
Firstname Lastnamezouxudong <your_email@youremail.com402253858@qq.com>
2015-05-10 17:49:01 +0800

    add list detail

--- public/js/config.js
+++ public/js/config.js
@@ -6,4 +6,11 @@ var appId = '7w25ol9vos571m9r9vz6cobpmi85smwyywenol0bhez3fbyb';
 var appKey = '2sz6a5fhupgvro5xqk0tbd4b6hogye5d25pgommibrq679e1';
 AV.initialize(appId, appKey);
 
+/*获取URL参数*/
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
 
