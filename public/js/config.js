db2c506781e04f96953c514f511e321b1966f213
Firstname Lastnamezouxudong <your_email@youremail.com402253858@qq.com>
2015-05-10 12:07:59 +0800

    add edito

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zouxudong on 15-5-10.
+ */
+
+var appId = '7w25ol9vos571m9r9vz6cobpmi85smwyywenol0bhez3fbyb';
+var appKey = '2sz6a5fhupgvro5xqk0tbd4b6hogye5d25pgommibrq679e1';
+AV.initialize(appId, appKey);
+
+
